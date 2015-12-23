c922f810b5879b42cdc1895d2cc00f7de536d5f2
Joshua Westgard <jwestgard@mac.com>
2015-12-23 17:29:29 -0500

    add regex transformations

--- transform.py
+++ transform.py
@@ -2,7 +2,7 @@
 import sys
 import os
 import re
-from lxml import etree
+import xml.etree.ElementTree as ET
 import json
 import argparse
 import codecs
@@ -40,7 +40,7 @@ def get_files_in_path(root):
 #================================================
 def verified_decode(f):
     encodings = ['ascii', 'utf-8', 'windows-1252', 'latin-1']
-    print("Checking encoding of {0}".format(f))
+    print("Checking encoding...")
     for encoding in encodings:
         bytes = codecs.open(f, mode='r', encoding=encoding, errors='strict')
         try:
@@ -60,11 +60,34 @@ def load_transformations(transform_file):
         return json.load(f)
 
 
-#=====================================
-# apply the regexes to the input file
-#=====================================
+#==================================
+# apply an individual find/replace
+#==================================
 def replace(match, replacement, text):
     return re.sub(match, replacement, text)
+
+
+#==============================================
+# apply the batch of regexes to the input file
+#==============================================
+def apply_regexes(text, transformations):
+    for t in transformations:
+        print(t['description'])
+        match = t['pattern']
+        replacement = t['replacement']
+    return re.sub(match, replacement, text)
+
+
+#=================================================
+# apply the xml transformations to the input file
+#=================================================
+def xml_transformations(filepath):
+    tree = ET.parse(filepath)
+    root = tree.getroot()
+    dsc = root.find('.//archdesc/dsc/[@type="in-depth"]')
+    
+    result = ET.tostring(root, encoding='utf-8')
+    return result
 
 
 #================
@@ -80,19 +103,16 @@ def main():
         input_dir = args.input
         print("Checking files in {0}...".format(input_dir))
         files_to_check = get_files_in_path(input_dir)
-        print(files_to_check)
     # otherwise, use arguments for files to check
     else:
         print(
             "No input path specified; processing files from arguments...")
         files_to_check = [os.path.basename(f) for f in args.files]
         input_dir = os.path.dirname(args.files[0])
-        print(files_to_check)
     # if resume flag set, remove files in outpath from files to check
     if args.resume:
         complete = get_files_in_path(output_dir)
         files_to_check = [f for f in files_to_check if f not in complete]
-        print(files_to_check)
     # if transform flag is set, load json transform file
     if args.transform:
         print("Loading regex transformations from file ...")
@@ -105,9 +125,9 @@ def main():
     for n, f in enumerate(files_to_check):
         print("\n{0}. Processing EAD file: {1}".format(n+1,f))
         input_path = os.path.join(input_dir, f)
-        print(input_path)
+        print("   IN => {0}".format(input_path))
         output_path = os.path.join(output_dir, f)
-        print(output_path)
+        print("   OUT => {0}".format(output_path))
         decoded_text = verified_decode(input_path)
         if not decoded_text:
             print("Could not reliably decode file, skipping...".format(f))
@@ -115,10 +135,14 @@ def main():
         if not args.encoding:
             # regex transformations
             print("Applying regexes...")
+            decoded_text = apply_regexes(decoded_text, transformations)
             # other transformations
             print("Applying XML transformations...")
-            root = etree.fromstring(decoded_text)
-            print(root)
+            # write out the decoded text to the output file
+            with codecs.open(output_path, 'w', encoding='utf8') as outfile:
+                outfile.write(decoded_text)
+            transformed_text = xml_transformations(output_path)
+
         else:
             # write out result
             with codecs.open(output_path, 'w', encoding='utf8') as outfile:
