c63e75d7f5bb2a892bde40a6b176ec2a5b130bbb
Joshua Westgard <jwestgard@mac.com>
2016-01-04 18:45:05 -0500

    refine overall structure and handle XML parsing errors

--- transform.py
+++ transform.py
@@ -21,6 +21,8 @@ def get_arguments():
         help='ouput path for transformed files')
     parser.add_argument('-r', '--resume', action='store_true', 
         help='resume job, skipping files that already exist in outpath')
+    parser.add_argument('-R', '--recursive', action='store_true', 
+        help='recursively process files starting at rootdirectory')
     parser.add_argument('-t', '--transform', 
         help="file containing regex transformations to apply")
     parser.add_argument('files', nargs='*', help='files to check')
@@ -30,9 +32,16 @@ def get_arguments():
 #========================================
 # get list of EAD files (input or output)
 #========================================
-def get_files_in_path(root):
-    return [f for f in os.listdir(root) if not f.startswith(
-        '.') and os.path.isfile(os.path.join(root, f))]
+def get_files_in_path(rootdir, recursive):
+    result = []
+    if recursive is True:
+        for (root, dir, files) in os.walk(rootdir):
+            result.extend(
+                [os.path.join(root, f) for f in files if not f.startswith('.')])
+    else:
+        result = [f for f in os.listdir(rootdir) if not f.startswith(
+            '.') and os.path.isfile(os.path.join(rootdir, f))]
+    return result
 
 
 #================================================
@@ -81,9 +90,8 @@ def apply_regexes(text, transformations):
 #=================================================
 # apply the xml transformations to the input file
 #=================================================
-def xml_transformations(filepath):
-    tree = ET.parse(filepath)
-    root = tree.getroot()
+def apply_transformations(text):
+    root = ET.fromstring(text)
     unitdates = root.findall('.//archdesc/did/unitdate')
     for u in unitdates:
         print(u.tag, u.attrib, u.text)
@@ -95,60 +103,80 @@ def xml_transformations(filepath):
 #  main function
 #================
 def main():
+    args = get_arguments()
     border = "=" * 19
     print("\n".join(['', border, "| EAD Transformer |", border]))
-    args = get_arguments()
-    output_dir = args.output
+    errors = []
+    
     # get files from inpath
     if args.input:
         input_dir = args.input
         print("Checking files in {0}...".format(input_dir))
-        files_to_check = get_files_in_path(input_dir)
+        files_to_check = get_files_in_path(input_dir, recursive=args.recursive)
     # otherwise, use arguments for files to check
     else:
+        input_dir = os.path.dirname(args.files[0])
         print(
             "No input path specified; processing files from arguments...")
         files_to_check = [os.path.basename(f) for f in args.files]
-        input_dir = os.path.dirname(args.files[0])
+    # set path for output
+    output_dir = args.output
+    
     # if resume flag set, remove files in outpath from files to check
     if args.resume:
-        complete = get_files_in_path(output_dir)
-        files_to_check = [f for f in files_to_check if f not in complete]
+        existing_files = get_files_in_path(output_dir, recursive=args.recursive)
+        files_to_check = [f for f in files_to_check if os.path.join(
+            os.path.relpath(f, input_dir), output_dir) not in existing_files]
+    
     # if transform flag is set, load json transform file
     if args.transform:
         print("Loading regex transformations from file ...")
         transformations = load_transformations(args.transform)
+    
     # if encoding flag is set
-    if args.encoding:
+    if args.encoding is True:
         print("-e flag set, will check encoding only...")
 
     # loop and process each file
     for n, f in enumerate(files_to_check):
-        print("\n{0}. Processing EAD file: {1}".format(n+1,f))
-        input_path = os.path.join(input_dir, f)
-        print("   IN => {0}".format(input_path))
-        output_path = os.path.join(output_dir, f)
+        # set up output paths and create directories if needed
+        output_path = os.path.join(output_dir, os.path.relpath(f, input_dir))
+        parent_dir = os.path.dirname(output_path)
+        if not os.path.isdir(parent_dir):
+            os.makedirs(parent_dir)
+        
+        # summarize file paths to screen
+        print("\n{0}. Processing EAD file: {1}".format(n+1, f))
+        print("   IN => {0}".format(f))
         print("   OUT => {0}".format(output_path))
-        decoded_text = verified_decode(input_path)
-        if not decoded_text:
+        
+        # attempt strict decoding of file according to common schemes
+        ead = verified_decode(f)
+        if not ead:
             print("Could not reliably decode file, skipping...".format(f))
+            errors.append("{0} could not be decoded.".format(f))
             continue
-        if not args.encoding:
-            # regex transformations
-            print("Applying regexes...")
-            decoded_text = apply_regexes(decoded_text, transformations)
-            # other transformations
-            print("Applying XML transformations...")
-            # write out the decoded text to the output file
-            with codecs.open(output_path, 'w', encoding='utf8') as outfile:
-                outfile.write(decoded_text)
-            transformed_text = xml_transformations(output_path)
 
+        if args.encoding is True:
+            # skip rest of loop if encoding-only flag is set
+            continue
         else:
-            # write out result
-            with codecs.open(output_path, 'w', encoding='utf8') as outfile:
-                outfile.write(decoded_text)
-
+            if args.transform is True:
+                # regex transformations
+                print("Applying regexes...")
+                ead = apply_regexes(ead, transformations)
+            else:
+                # other transformations
+                print("Applying XML transformations...")
+                try:
+                    ead = apply_transformations(ead)
+                except ET.ParseError as e:
+                    errors.append("{0} malformed ({1} at {2})".format(
+                        f, e.code, e.position))
+                        
+        # write out result
+        with open(output_path, 'w') as outfile:
+            outfile.write(str(ead))
 
 if __name__ == '__main__':
     main()
